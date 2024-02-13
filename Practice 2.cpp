03ccbd76cacc6d16f2d7d548685f5a5c2a75a6e3
aminsani313 <159550312+aminsani313@users.noreply.github.com>
2024-02-13 14:23:02 +0330

    Update and rename Practice 2 to Practice 2.cpp

--- Practice 2.cpp
+++ Practice 2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    // Amin Saniei ASCII Codes : 65 109 105 110 83 97 110 105 101 105
+ Amin Saniei ASCII Codes : 65 109 105 110 83 97 110 105 101 105
     cout << char(65) << char(109) << char(105) << char(110) << char(83) << char(97) << char(110) << char(105) << char(101) << char(105)"\n";
     return 0;
 }
